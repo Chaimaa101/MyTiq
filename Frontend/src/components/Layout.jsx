bfad3c61326b8cda87d81a94fa4d42bdecb3cce3
Sara-BELKACEM <Sarabelkacem879@gmail.com>
2025-12-01 14:50:27 +0100

    Modification

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from './Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import Navbar from './Navbar'
 
 function Layout() {
   return (
     <>
-    <Header/>
+    <Navbar/>
         <Outlet />
     <Footer/>
     </>
